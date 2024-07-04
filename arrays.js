ba6e9636a143df06842fa890a79e1c22b0d436b7
ISuperUseRI <mikeadhil2002@gmail.com>
2024-07-05 04:18:24 +0530

    js files uploaded

---
+++ arrays.js
@@ -0,0 +1,26 @@
+const items = ["mike" ,"ben" , "huthoo"]
+
+console.log(items);
+
+
+items.push("add1" , "add2")
+
+console.log(items);
+
+items.unshift("this is the first One")
+
+console.log(items);
+
+items.splice(1, 0 , "this is added to 2nd eliment")
+
+
+console.log(items);
+
+
+
+//   search in the array 
+// if array contains the element that will show the index else it will return -1
+
+
+console.log(items.indexOf("mike") !== -1);
+
---
+++ let.js
@@ -0,0 +1,18 @@
+let myName = "mike" 
+
+
+function changeName () {
+     myName = "ben" 
+
+    console.log(myName)
+}
+
+changeName() ;
+console.log(myName);
+
+
+// let currenttime = 900 ;
+
+//  currenttime = 1000; 
+
+//  console.log(currenttime);
